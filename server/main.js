f281da5c099379cd2a486dc18ec1382160bf969d
LesPantsFancy <mjnrock@umich.edu>
2023-12-21 12:10:48 -0500

    wip

--- server/main.js
+++ server/main.js
@@ -4,30 +4,43 @@ import expressWs from "express-ws";
 import cors from "cors";
 import fs from "fs/promises";
 import https from "https";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-export async function main() {
+import DMA200 from "./sandbox/200DMA.js";
+import UploadData from "./sandbox/UploadData.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export async function setup() {
 	// Read the certificate files
 	const privateKey = await fs.readFile("./certs/kiszka.key", "utf8");
 	const certificate = await fs.readFile("./certs/kiszka.crt", "utf8");
-
+	
 	// Create a credentials object using the private key and certificate
 	const credentials = { key: privateKey, cert: certificate };
-
+	
 	const app = express();
 	const ws = expressWs(app);
-
+	
 	app.use(cors());
-
+	
 	app.get("/", (req, res) => {
 		res.json({
 			message: "Hello World!",
 		})
 	});
-
+	
 	const httpsServer = https.createServer(credentials, app);
 	httpsServer.listen(process.env.PORT, () => {
 		console.log(`[${ Date.now() }]: Server is running on port: ${ process.env.PORT }`);
 	});
 };
+export async function main() {
+	await setup();
+
+	// DMA200(__dirname);
+	UploadData(__dirname);
+}
 
 main();
