6651319b7928d9ee001669c90c6814c507d8e07f
LesPantsFancy <mjnrock@umich.edu>
2023-12-23 23:39:38 -0500

    wip

--- server/main.js
+++ server/main.js
@@ -100,36 +100,36 @@ export async function main() {
 				cols: [ [ "close" ], [ "close" ], [ "close" ], [ "close" ], [ "close" ] ],
 				args: [ [ 7, 11, 5 ], [ 12, 19, 8 ], [ 12, 26, 9 ], [ 20, 33, 14 ], [ 33, 53, 22 ] ]  // Baseline: 12, 26, 9
 			},
-			{
-				fn: "bollinger",
-				cols: [ [ "close" ], [ "close" ], [ "close" ], [ "close" ], [ "close" ] ],
-				args: [ [ 8, 2 ], [ 13, 2 ], [ 20, 2 ], [ 32, 2 ], [ 52, 2 ] ]  // Baseline: 20, 2
-			},
-			{
-				fn: "stoch",
-				cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
-				args: [ [ 5, 3, 3 ], [ 9, 3, 3 ], [ 14, 3, 3 ], [ 23, 3, 3 ], [ 37, 3, 3 ] ]  // Baseline: 14, 3, 3
-			},
-			{
-				fn: "willr",
-				cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
-				args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
-			},
-			{
-				fn: "adx",
-				cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
-				args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
-			},
-			{
-				fn: "atr",
-				cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
-				args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
-			},
-			{
-				fn: "cci",
-				cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
-				args: [ [ 8 ], [ 13 ], [ 20 ], [ 32 ], [ 52 ] ]  // Baseline: 20
-			}
+			// {
+			// 	fn: "bollinger",
+			// 	cols: [ [ "close" ], [ "close" ], [ "close" ], [ "close" ], [ "close" ] ],
+			// 	args: [ [ 8, 2 ], [ 13, 2 ], [ 20, 2 ], [ 32, 2 ], [ 52, 2 ] ]  // Baseline: 20, 2
+			// },
+			// {
+			// 	fn: "stoch",
+			// 	cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
+			// 	args: [ [ 5, 3, 3 ], [ 9, 3, 3 ], [ 14, 3, 3 ], [ 23, 3, 3 ], [ 37, 3, 3 ] ]  // Baseline: 14, 3, 3
+			// },
+			// {
+			// 	fn: "willr",
+			// 	cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
+			// 	args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
+			// },
+			// {
+			// 	fn: "adx",
+			// 	cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
+			// 	args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
+			// },
+			// {
+			// 	fn: "atr",
+			// 	cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
+			// 	args: [ [ 5 ], [ 9 ], [ 14 ], [ 23 ], [ 37 ] ]  // Baseline: 14
+			// },
+			// {
+			// 	fn: "cci",
+			// 	cols: [ [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ], [ "high", "low", "close" ] ],
+			// 	args: [ [ 8 ], [ 13 ], [ 20 ], [ 32 ], [ 52 ] ]  // Baseline: 20
+			// }
 		],
 		delay: 0,
 	});
--- server/modules/node/technical-analysis/ProcessTechnicalIndicators.crypto.js
+++ server/modules/node/technical-analysis/ProcessTechnicalIndicators.crypto.js
@@ -1,6 +1,117 @@
 import tulind from "tulind";
 import { DataSet } from "../data-set/DataSet.js";
 
+/**
+ * Basic mapping of indicator names to output columns
+ */
+export const INDICATOR_OUTPUT_COLUMNS = {
+	abs: [ "value" ],
+	acos: [ "value" ],
+	ad: [ "value" ],
+	add: [ "value" ],
+	adosc: [ "value" ],
+	adx: [ "value" ],
+	adxr: [ "value" ],
+	ao: [ "value" ],
+	apo: [ "value" ],
+	aroon: [ "up", "down" ],
+	aroonosc: [ "value" ],
+	asin: [ "value" ],
+	atan: [ "value" ],
+	atr: [ "value" ],
+	avgprice: [ "value" ],
+	bbands: [ "lower", "middle", "upper" ],
+	bop: [ "value" ],
+	cci: [ "value" ],
+	ceil: [ "value" ],
+	cmo: [ "value" ],
+	cos: [ "value" ],
+	cosh: [ "value" ],
+	crossany: [ "value" ],
+	crossover: [ "value" ],
+	cvi: [ "value" ],
+	decay: [ "value" ],
+	dema: [ "value" ],
+	di: [ "plus", "minus" ],
+	div: [ "value" ],
+	dm: [ "plus", "minus" ],
+	dpo: [ "value" ],
+	dx: [ "value" ],
+	edecay: [ "value" ],
+	ema: [ "value" ],
+	emv: [ "value" ],
+	exp: [ "value" ],
+	fisher: [ "fisher", "signal" ],
+	floor: [ "value" ],
+	fosc: [ "value" ],
+	hma: [ "value" ],
+	kama: [ "value" ],
+	kvo: [ "value" ],
+	lag: [ "value" ],
+	linreg: [ "value" ],
+	linregintercept: [ "value" ],
+	linregslope: [ "value" ],
+	ln: [ "value" ],
+	log10: [ "value" ],
+	macd: [ "macd", "signal", "histogram" ],
+	marketfi: [ "value" ],
+	mass: [ "value" ],
+	max: [ "value" ],
+	md: [ "value" ],
+	medprice: [ "value" ],
+	mfi: [ "value" ],
+	min: [ "value" ],
+	mom: [ "value" ],
+	msw: [ "sine", "leadSine" ],
+	mul: [ "value" ],
+	natr: [ "value" ],
+	nvi: [ "value" ],
+	obv: [ "value" ],
+	ppo: [ "value" ],
+	psar: [ "value" ],
+	pvi: [ "value" ],
+	qstick: [ "value" ],
+	roc: [ "value" ],
+	rocr: [ "value" ],
+	round: [ "value" ],
+	rsi: [ "value" ],
+	sin: [ "value" ],
+	sinh: [ "value" ],
+	sma: [ "value" ],
+	sqrt: [ "value" ],
+	stddev: [ "value" ],
+	stderr: [ "value" ],
+	stoch: [ "stochK", "stochD" ],
+	stochrsi: [ "value" ],
+	sub: [ "value" ],
+	sum: [ "value" ],
+	tan: [ "value" ],
+	tanh: [ "value" ],
+	tema: [ "value" ],
+	todeg: [ "value" ],
+	torad: [ "value" ],
+	tr: [ "value" ],
+	trima: [ "value" ],
+	trix: [ "value" ],
+	trunc: [ "value" ],
+	tsf: [ "value" ],
+	typprice: [ "value" ],
+	ultosc: [ "value" ],
+	var: [ "value" ],
+	vhf: [ "value" ],
+	vidya: [ "value" ],
+	volatility: [ "value" ],
+	vosc: [ "value" ],
+	vwma: [ "value" ],
+	wad: [ "value" ],
+	wcprice: [ "value" ],
+	wilders: [ "value" ],
+	willr: [ "value" ],
+	wma: [ "value" ],
+	zlema: [ "value" ]
+};
+
+
 export class ProcessTechnicalIndicators {
 	constructor ({ state = {} } = {}) {
 		this.state = {
@@ -48,10 +159,14 @@ export class ProcessTechnicalIndicators {
 					// Calculate the starting index for the data to align with the indicator results
 					const startIndex = inputs[ 0 ].length - indicatorResults[ 0 ].length;
 
-					const resultData = indicatorResults[ 0 ].map((value, index) => ({
-						date: input.data[ startIndex + index ].date,
-						value
-					}));
+					const outputCols = INDICATOR_OUTPUT_COLUMNS[ indicatorName ];
+					const resultData = input.data.slice(startIndex).map((item, index) => {
+						let resultEntry = { date: item.date };
+						outputCols.forEach((col, colIndex) => {
+							resultEntry[ col ] = indicatorResults[ colIndex ][ index ];
+						});
+						return resultEntry;
+					});
 
 					//STUB: Partial stub, crypto specific -- abstract this kind of thing
 					let nextMeta = {
