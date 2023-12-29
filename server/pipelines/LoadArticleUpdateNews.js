90d8a2968aa6d51ecbf5cfaa83dff6bee165d0e4
LesPantsFancy <mjnrock@umich.edu>
2023-12-28 20:57:29 -0500

    wip

--- server/pipelines/LoadArticleUpdateNews.js
+++ server/pipelines/LoadArticleUpdateNews.js
@@ -11,7 +11,7 @@ export async function LoadArticleUpdateNews({ symbols = [], context = {} } = {})
 			ModNode.Lib.DataSource.FileDataSource.Create({
 				state: {
 					path: "./data/news",
-					file: `${ symbol }.json`,
+					file: `${ symbol }.news.ds`,
 				},
 			}),
 			async (input) => {
@@ -22,16 +22,26 @@ export async function LoadArticleUpdateNews({ symbols = [], context = {} } = {})
 					const { uuid } = record;
 					const articleFileName = path.join(process.cwd(), `./data/news/content/${ symbol }-${ uuid }.article`);
 
+					console.log(symbol, uuid, articleFileName);
+
 					try {
-						const articleContent = JSON.parse(await fs.readFile(articleFileName, "utf8"));
-						data[ i ].text = articleContent;
+						const articleContent = await fs.readFile(articleFileName, "utf8");
+						const article = JSON.parse(articleContent);
+
+						data[ i ].article = article;
 					} catch(error) {
-						// console.error(`Error loading article for ${ symbol }-${ uuid }:`, error.message);
+						console.error(`Error loading article for ${ symbol }-${ uuid }:`, error.message);
 					}
 				}
 
 				return data;
 			},
+			ModNode.Lib.DataDestination.FileDataDestination.Create({
+				state: {
+					path: "./data/news",
+					file: `${ symbol }.news.ds`,
+				},
+			}),
 		]);
 
 		results.push(await pipeline.run({
